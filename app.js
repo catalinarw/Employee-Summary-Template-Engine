47ce79570a57b4359201e00ab7a61fbb1d51e54f
Catalina Williams <catalina.rose1361@gmail.com>
2020-03-08 17:09:39 -0600

    intern.js passes test, pseudocode added for app.js

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ const outputPath = path.resolve(__dirname, "output", "team.html");
 const render = require("./lib/htmlRenderer");
 
 // prompts go here somehwere, and functions
+
+// prompt user for info about the team manager
+    // inquirer
+// prompt user for information about a team member
+    //would you like to add another team member?
+    // if yes prompt user for information about the new team member
+    // if no, 
+    //.then(data) use the data collected to seed into html files
